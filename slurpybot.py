8bec95445548e1dc61ce689b02ca8e0e0ef58c71
Rayyan Ahmed <rayyanahmed747@gmail.com>
2024-08-03 04:32:02 -0600

    Added some Fortnite API capabilties. Latest news, latest item shop and player stats lookup.

--- slurpybot.py
+++ slurpybot.py
@@ -1,9 +1,11 @@
 import discord
 from discord.ext import commands
-from jokeapi import Jokes
 import apikeys
 import requests
+import math
 from datetime import datetime
+from jokeapi import Jokes
+import fortnite_api
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,30 +13,24 @@ intents.members = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
+
+# BASICS
+
 @client.event
 async def on_ready():
     print("BOT ONLINE")
 
 @client.event
 async def on_member_join(member):
-
-    joke_api = await Jokes()
-    joke = await joke_api.get_joke()
-    if joke["type"] == "single":
-        joke_text = joke["joke"]
-    else:
-        joke_text = f"{joke['setup']}\n{joke['delivery']}"
-
     channel = client.get_channel(apikeys.GENERAL_ID)
     if channel:
-        await channel.send(f'Welcome {member.name}!')
-        await channel.send(joke_text)
+        await channel.send(f'**Welcome {member.name}!**')
 
 @client.event
 async def on_member_remove(member):
     channel = client.get_channel(apikeys.GENERAL_ID)
     if channel:
-        await channel.send(f'Goodbye {member.name}... 🚬')
+        await channel.send(f'**Goodbye {member.name}... 🚬**')
 
 @client.command()
 async def hello(ctx):
@@ -46,6 +42,9 @@ async def abc(ctx):
     print("COMMAND RECIEVED")
     await ctx.send("123")
 
+
+
+# JOKE
 @client.command()
 async def joke(ctx):
     print("COMMAND RECIEVED")
@@ -55,18 +54,20 @@ async def joke(ctx):
     if joke["type"] == "single":
         joke_text = joke["joke"]
     else:
-        joke_text = f"{joke['setup']}\n{joke['delivery']}"
+        joke_text = f"**{joke['setup']}**\n*{joke['delivery']}*"
 
     await ctx.send(joke_text)
 
+
+
+# WEATHER
 @client.command()
 async def weather(ctx, *, city: str):
     print("COMMAND RECIEVED")
     
+    weather_api = weather_api = f"http://api.weatherapi.com/v1/current.json?key={apikeys.WEATHER_API}&q={city}"
 
-    url = url = f"http://api.weatherapi.com/v1/current.json?key={apikeys.WEATHER_API}&q={city}"
-
-    response = requests.get(url)
+    response = requests.get(weather_api)
     data = response.json()
 
     city_name = data['location']['name']
@@ -86,18 +87,128 @@ async def weather(ctx, *, city: str):
     local_time_formatted = datetime.strptime(local_time, '%Y-%m-%d %H:%M').strftime('%B %d, %Y  |  %I:%M %p')
     last_updated_formatted = datetime.strptime(last_updated, '%Y-%m-%d %H:%M').strftime('%B %d, %Y  |  %I:%M %p')
 
-    weather_info =  (f'{city_name}, {country} 🗺️\n'
-                    f'Local Timestamp: {local_time_formatted} 🕒\n'
+    weather_info =  (f'**{city_name}, {country} 🗺️**\n'
+                    f'**Local Timestamp:** {local_time_formatted} 🕒\n'
                     f'\n'
-                    f'Last Update: {last_updated_formatted} 🕒\n'
-                    f'Temperature: {temp_c}°C / {temp_f}°F 🌡️\n'
-                    f'Condition: {condition} 🌥️\n'
-                    f'Wind: {wind_kph} kph / {wind_mph} mph 💨\n'
-                    f'Precipitation: {precip_mm} mm / {precip_in} in 💧\n'
-                    f'Humidity: {humidity}% ☀️')
+                    f'**Last Update:** {last_updated_formatted} 🕒\n'
+                    f'**Temperature:** {temp_c}°C / {temp_f}°F 🌡️\n'
+                    f'**Condition:** {condition} 🌥️\n'
+                    f'**Wind:** {wind_kph} kph / {wind_mph} mph 💨\n'
+                    f'**Precipitation:** {precip_mm} mm / {precip_in} in 💧\n'
+                    f'**Humidity:** {humidity}% ☀️')
     
     await ctx.send(weather_info)
+
+
+
+# FORTNITE
+fort_api = fortnite_api.FortniteAPI(api_key=apikeys.FORTNITE_API)
+
+def chunk_message(message, chunk_size=2000):
+    for i in range(0, len(message), chunk_size):
+        yield message[i:i + chunk_size]
+
+@client.command()
+async def news(ctx):
+    print("COMMAND RECIEVED")
+
+    news_data = fort_api.news.fetch()
+    br_news = news_data.br.motds
+    news_date = news_data.br.date.strftime('%B %d, %Y') 
+
+    news = []
+    news.append(f'**{news_date}  🗞️**')
+    for item in br_news:
+        news_item = f"**{item.title}**\n{item.body}"
+        news.append(news_item)
+
+    await ctx.send("\n\n".join(news))
+
+@client.command()
+async def shop(ctx):
+    print("COMMAND RECEIVED")
+
+    shop_data = fort_api.shop.fetch()
+    shop_date = shop_data.date.strftime('%B %d, %Y')
+    daily_items = shop_data.daily.entries if shop_data.daily else []
+    featured_items = shop_data.featured.entries if shop_data.featured else []
+
+    categories = {}
+    seen_skins = {}
+    shop = []
+    shop.append(f'**Shop Date:** {shop_date}  🛍️\n')
+
+
+    # Check if skin is a duplicate/bundle, then add to seen skins dictionary with category, skin name and price
+    def add_to_category(category_name, cosmetic_name, price):
+        if cosmetic_name not in seen_skins or price < seen_skins[cosmetic_name]['price']:
+            seen_skins[cosmetic_name] = {'price': price, 'category': category_name}
+            # seen_skins = comestic_name: {price: price, category: category_name}
+
+    for item in featured_items:
+        category_name = item.layout.name
+        for cosmetic in item.items:
+            if cosmetic.type.value == 'outfit':
+                add_to_category(category_name, cosmetic.name, item.final_price)
+                
+    for item in daily_items:
+        category_name = 'Daily'
+        for cosmetic in item.items:
+            if cosmetic.type.value == 'outfit':
+                add_to_category(category_name, cosmetic.name, item.final_price)
+
+    # Check the category from seen skins and adds it to the categories dictionary
+    for skin, data in seen_skins.items():
+        if data['category'] not in categories:
+            categories[data['category']] = []
+        categories[data['category']].append(f"{skin} ({data['price']})")
+        # Add the skin and price values from seen skins to the categories dictionary
+
+    for category, items in categories.items():
+        category_item = f'**{category}** - {", ".join(items)}\n'
+        shop.append(category_item)
+
+    message = '\n'.join(shop)
+    for chunk in chunk_message(message):
+        await ctx.send(chunk)
+
+@client.command()
+async def stats(ctx, player_name: str):
+    print("COMMAND RECIEVED")
+
+    try:
+        stats_data = fort_api.stats.fetch_by_name(name=player_name)
+        battle_pass = stats_data.battle_pass
+        overall_stats = stats_data.stats.all.overall
+
+        wins = math.floor(overall_stats.matches * (overall_stats.win_rate / 100))
+        hours_played = math.floor(overall_stats.minutes_played / 60)
+
+        stats_message = (
+            f"**{player_name}'s Fortnite Stats  🔬**\n"
+            f"Season Level: {battle_pass.level}\n"
+            f"Matches Played: {overall_stats.matches}\n"
+            f"Time Played: {hours_played} hours\n"
+            f"Wins: {wins}\n"
+            f"Win Rate: {overall_stats.win_rate:.3f}%\n"
+            f"Kills: {overall_stats.kills}\n"
+            f"Deaths: {overall_stats.deaths}\n"
+            f"K/D: {overall_stats.kd}\n"
+        )
+
+        await ctx.send(stats_message)
+    
+    except fortnite_api.errors.Forbidden:
+        await ctx.send(f"{player_name}'s stats are not public. 🐱")
+    except fortnite_api.errors.NotFound:
+        await ctx.send(f"{player_name}'s account does not exist.  👻")
+
+# 📍
+@client.command()
+async def map(ctx):
+    print("COMMAND RECIEVED")
+
+
                     
 
 client.run(apikeys.BOT_TOKEN)
-
